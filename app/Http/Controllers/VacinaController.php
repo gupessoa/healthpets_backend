302b11b600164957b2c3a83f4c1a31b8202673ae
Gustavo Pessoa <gupessoa@live.com>
2022-01-12 11:03:59 -0300

    Criando model, request, migration, controller de Vacinas

---
+++ app/Http/Controllers/VacinaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVacinaRequest;
+use App\Http\Requests\UpdateVacinaRequest;
+use App\Models\Vacina;
+
+class VacinaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreVacinaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreVacinaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vacina  $vacina
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vacina $vacina)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vacina  $vacina
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vacina $vacina)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateVacinaRequest  $request
+     * @param  \App\Models\Vacina  $vacina
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateVacinaRequest $request, Vacina $vacina)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vacina  $vacina
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vacina $vacina)
+    {
+        //
+    }
+}
