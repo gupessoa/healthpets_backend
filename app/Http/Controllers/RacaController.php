a78bcc2adfb59948f4a9520452572ea7befdc9f3
Gustavo Pessoa <gupessoa@live.com>
2022-02-20 19:38:12 -0300

    Criando os recursos de Raças e implementando migration e model do mesmo

---
+++ app/Http/Controllers/RacaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RacaRequest;
+use App\Http\Requests\UpdateRacaRequest;
+use App\Models\Raca;
+
+class RacaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\RacaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(RacaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(int $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\RacaRequest  $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(RacaRequest $request, int $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(int $id)
+    {
+        //
+    }
+}
