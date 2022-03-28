067c226412e2e0a49ae50d01ad1fbb0211c4c6c1
Gustavo Pessoa <gupessoa@live.com>
2022-03-27 23:01:29 -0300

    Adicionando a verificação de segurança no controller animal 2

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -17,7 +17,7 @@ class AnimalController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api');
+//        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
     /**
