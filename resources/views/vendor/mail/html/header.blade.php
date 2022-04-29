6cab7294db385d4300626aa0a9dc5e2ed3740640
Gustavo Pessoa <gupessoa@live.com>
2022-04-29 17:36:41 -0300

    Adicionando arquivos e imagens de template de email

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,12 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@else
+        <img src=<?php echo $message->embed(asset("images/logo_health_pets.png")) ?> class="logo" alt="Logo Healthpets">
+        <p>{{ asset("images/logo_health_pets.png") }}</p>
+@endif
+</a>
+</td>
+</tr>
