ef248eeb18394f323112ab0d39cb9155c754a22f
Gustavo Pessoa <gupessoa@live.com>
2022-05-18 21:13:00 -0300

    BugFix error mail

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,6 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/', function(){
-//    return view('notifications::email');return new \App\Mail\VerifyEmail(1234789521);
+//    return view('notifications::email');
+//return new \App\Mail\VerifyEmail(1234789521);
 });
